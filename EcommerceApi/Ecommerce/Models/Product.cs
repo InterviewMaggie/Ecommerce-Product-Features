aa8e885d4dff49449ebdadef1dbd54dcebaa73b2
munyite-megzs <margaret.sitati1@gmail.com>
2021-06-11 01:56:14 +0300

    Added Product Model

---
+++ EcommerceApi/Ecommerce/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Models
+{
+    public class Product
+    {/*
+        id int NOT NULL,
+	productName nvarchar(250) NOT NULL,
+    category_id int NOT NULL,
+	subcategory_id int NOT NULL,
+	PreviewImages nvarchar NOT NULL,*/
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public DateTime Created { get; set; }
+        [Required]
+        public string PreviewImage { get; set; }
+    }
+}
