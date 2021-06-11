86f99b7b13d35478603518559167da1c1154dffd
munyite-megzs <margaret.sitati1@gmail.com>
2021-06-11 04:08:49 +0300

    Added Automapper to map dto and models

---
+++ EcommerceApi/Ecommerce/Mapper/EcommerceMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using EcommerceAPI.DTO;
+using EcommerceAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Mapper
+{
+    public class EcommerceMappings : Profile
+    {
+        public EcommerceMappings()
+        {
+            
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
--- EcommerceApi/Ecommerce/Startup.cs
+++ EcommerceApi/Ecommerce/Startup.cs
@@ -2,17 +2,12 @@ using EcommerceAPI.Data;
 using EcommerceAPI.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using EcommerceAPI.Mapper;
 
 namespace Ecommerce
 {
@@ -31,6 +26,7 @@ namespace Ecommerce
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
             );
+            services.AddAutoMapper(typeof(EcommerceMappings));
 
             services.AddScoped<IProductsRepository, ProductsRepository>();
             services.AddControllers();
